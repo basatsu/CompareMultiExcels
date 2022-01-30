7f3fd4fe11c1c31a8197021ffb9abdae03d63715
basatsu <tubaxile@gmail.com>
2022-01-30 18:21:25 +0900

    jenkinsれんテスト8

--- DiffMultiExcels.py
+++ DiffMultiExcels.py
@@ -14,7 +14,7 @@ oldbook = input('Old folder pass:')
 newbook = input('New folder pass:')
 output = input('Output diff.xlsx to which folder pass:')
 outtext = input('Output diff.txt to which folder pass:')
-outsheet = input('when not same sheets,which folder pass to record?:')
+outsheet = input('Output diff.xlsx to which folder pass(when not same sheets):')
 
 for old in glob.glob(os.path.join(oldbook, '*.xlsx')): 
     for new in glob.glob(os.path.join(newbook, '*.xlsx')):
